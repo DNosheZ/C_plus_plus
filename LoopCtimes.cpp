590524d69d9bff1c40615152e04ddd0c51ed4c6b
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-06-07 19:27:26 -0500

    Create LoopCtimes.cpp

---
+++ LoopCtimes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+//se debe incluir las librerias para cada tipo de arbol
+using namespace std;
+
+int main(){
+
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int h; cin>>h;
+  
+  for(int i=0; i<=h; i++){
+  
+  };
+
+}
